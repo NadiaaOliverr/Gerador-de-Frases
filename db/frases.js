6dbcd79d179d1d3b54ab97556e8e09be9a48e6f6
Nádia Oliveira <nadiaaoliverr@gmail.com>
2023-10-13 14:49:34 -0300

    Organizando o código e módulos

---
+++ db/frases.js
@@ -0,0 +1,132 @@
+const quotes = [
+    {
+      quote: "A persistência é o caminho do êxito.",
+      author: "Charles Chaplin",
+    },
+    {
+        quote: "Nada é permanente, exceto a mudança.",
+        author: "Heráclito de Éfeso",
+    },
+    {
+      quote: "A única forma de fazer um grande trabalho é amar o que se faz.",
+      author: "Steve Jobs",
+    },
+    {
+        quote: "Uma vida sem reflexão não vale a pena ser vivida",
+        author: "Sócrates",
+    },
+    {
+        quote: "Felicidade é aquele momento que você quer que nunca acabe, e que quando acabar, você desejará que ele retorne.",
+        author: "Clóvis de Barros Filho",
+    },
+    {
+        quote: "É preciso ter sonhos. Se não tivermos sonhos, como teremos sonhos realizados?",
+        author: "C.J.Tudor",
+    },
+    {
+        quote: "A esperança é como o sal, não alimenta, mas dá sabor ao pão.",
+        author: "José Saramago",
+    },
+    {
+        quote: "As perguntas são mais importantes do que as respostas.",
+        author: "Autor desconhecido",
+    },
+    {
+        quote: "A coisa mais difícil é a decisão de agir, o resto é apenas tenacidade",
+        author: "Amelia Earhart",
+    },
+    {
+        quote: "Esforce-se para não ser um sucesso, mas sim para ser valioso",
+        author: "Albert Einstein",
+    },
+    {
+        quote: "Seu tempo é limitado, então, não o gaste vivendo a vida de outra pessoa",
+        author: "Steve Jobs",
+    },
+    {
+        quote: "Ganhar não é tudo, mas querer ganhar é",
+        author: "Vince Lombardi",
+    },
+    {
+        quote: "Você nunca pode atravessar o oceano até que tenha coragem de perder de vista a costa",
+        author: "Cristóvão Colombo",
+    },
+    {
+        quote: "Os dias mais importantes na sua vida são o dia em que você nasce e o dia em que descobre o porquê",
+        author: "Mark Twain",
+    },
+    {
+      quote: "O sucesso é a soma de pequenos esforços repetidos dia após dia.",
+      author: "Robert Collier",
+    },
+    {
+      quote:
+        "O fracasso é a oportunidade de começar de novo, com mais experiência.",
+      author: "Henry Ford",
+    },
+    {
+      quote: "Não espere por oportunidades, crie você mesmo as suas.",
+      author: "Autor desconhecido",
+    },
+    {
+      quote:
+        "O verdadeiro sucesso não é o sucesso financeiro, mas sim o sucesso em ajudar outras pessoas.",
+      author: "Zig Ziglar",
+    },
+    {
+      quote:
+        "O sucesso não é final, o fracasso não é fatal: o que importa é ter coragem para continuar.",
+      author: "Winston Churchill",
+    },
+    {
+      quote:
+        "O sucesso consiste em ir de fracasso em fracasso sem perder o entusiasmo.",
+      author: "Winston Churchill",
+    },
+    {
+        quote:
+          "Tudo o que você sempre quis está do outro lado do seu medo",
+        author: "George Addair",
+    },
+    {
+        quote:
+          "Caia 7 vezes. Levante-se 8",
+        author: "Provérbio japonês",
+    },
+    {
+        quote:
+          "A felicidade não é algo pronto. Ela vem de suas próprias ações",
+        author: "Dalai Lama",
+    },
+    {
+        quote:
+          "Você só cresce quando ouve aquilo que não quer ouvir",
+        author: "Luiza Trajano",
+    },
+    {
+        quote:
+          "Antes que diga que não consegue fazer alguma coisa, experimente.",
+        author: "Sakichi Toyoda",
+    },
+    {
+        quote:
+          "O que sabemos é uma gota; o que ignoramos é um oceano.",
+        author: "Isaac Newton",
+    },
+    {
+        quote:
+          "Faça as coisas o mais simples que você puder, porém não se restrinja às mais simples.",
+        author: "Albert Einstein",
+    },
+    {
+      quote: "Acredite em si próprio e todo o resto virá naturalmente.",
+      author: "Elon Musk",
+    },
+    {
+      quote:
+        "O fracasso não significa que você não seja bom o suficiente, significa que você precisa se esforçar mais.",
+      author: "Autor desconhecido",
+    },
+];
+
+export default quotes;
